cc72618d3cdc66549c6b9876ba7ee088e9a9bb6d
fernandajuliettepasillasgonzalez <fernanda_pasillas24@unikino.edu.mx>
2024-09-10 07:22:15 -0600

    Add project files.

---
+++ Practica2/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Practica2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //declarando variable
+            string nombre;
+            string apellido;
+            int edad=15;
+
+
+
+            Console.WriteLine("Introduce tu nombre: ");
+            nombre = Console.ReadLine();
+            Console.WriteLine("Introduce tu apellido: ");
+            apellido = Console.ReadLine();
+            Console.WriteLine(nombre + " " + apellido + " " + (1 + edad));
+            Console.WriteLine((1 + 3) * 5m / 2);
+        }
+    }
+}
